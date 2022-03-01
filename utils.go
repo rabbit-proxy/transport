3cdfc1cae60fca5d65bc6d34bc5b0852ff46d49d
Jack King <email@qiaohong.org>
2022-03-01 17:10:41 +0800

    feat: remove Relay method

--- utils.go
+++ utils.go
@@ -1,10 +1,7 @@
 package transport
 
 import (
-	"go.uber.org/zap"
-	"io"
 	"math/rand"
-	"net"
 	"sync"
 	"time"
 )
@@ -15,7 +12,7 @@ func init() {
 
 const (
 	BufferLength = 16 * 1024
-	BufferLimit  = 12 * 1024 // 保留一部分用作 header
+	BufferLimit  = 15 * 1024 // 保留一部分用作 header
 )
 
 var (
@@ -38,33 +35,4 @@ func GetBuffer() []byte {
 
 func PutBuffer(buffer []byte) {
 	bufferPool.Put(buffer)
-}
-
-func Relay(reader, writer io.ReadWriteCloser) {
-	buffer := GetBuffer()
-	defer PutBuffer(buffer)
-
-	for {
-		readerConn, ok := reader.(net.Conn)
-		if ok {
-			err := readerConn.SetReadDeadline(time.Now().Add(8 * time.Second)) // 默认读取超时时间为八秒
-			if err != nil {
-				zap.L().Debug("reader conn set read deadline err", zap.Error(err))
-				return
-			}
-			reader = readerConn
-		}
-
-		n, err := reader.Read(buffer[:BufferLimit])
-		if err != nil {
-			zap.L().Debug("err on read reader msg", zap.Error(err))
-			return
-		}
-
-		_, err = writer.Write(buffer[:n])
-		if err != nil {
-			zap.L().Debug("err on write writer msg", zap.Error(err))
-			return
-		}
-	}
 }
