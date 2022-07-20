0b3a7cfe5270889bee1359fe4c2b3b773228dece
Jack King <email@qiaohong.org>
2022-07-21 03:51:56 +0800

    refactor: simplify interface

--- constant.go
+++ constant.go
@@ -1,70 +1,17 @@
 package transport
 
 import (
-	"context"
-	"net"
-	"time"
+	"io"
 )
 
-type SocketInterface interface {
-	Read(ctx context.Context, data []byte) (int, error)
-	Write(ctx context.Context, data []byte) (int, error)
-	Close() error
+// CryptConnType 加密连接
+type CryptConnType struct {
+	conn  io.ReadWriteCloser
+	crypt Encryption
 }
 
-type ConnSocketType struct {
-	conn net.Conn
-}
-
-func (c *ConnSocketType) Read(ctx context.Context, data []byte) (int, error) {
-	readCtx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	go func() {
-		select {
-		case <-ctx.Done():
-			_ = c.conn.SetReadDeadline(time.Now())
-		case <-readCtx.Done():
-			return
-		}
-	}()
-
-	return c.conn.Read(data)
-}
-
-func (c *ConnSocketType) Write(ctx context.Context, data []byte) (int, error) {
-	writeCtx, cancel := context.WithCancel(context.TODO())
-	defer cancel()
-
-	go func() {
-		select {
-		case <-ctx.Done():
-			_ = c.conn.SetWriteDeadline(time.Now())
-		case <-writeCtx.Done():
-			return
-		}
-	}()
-	return c.conn.Write(data)
-}
-
-func (c *ConnSocketType) Close() error {
-	return c.conn.Close()
-}
-
-func NewConnSocket(conn net.Conn) *ConnSocketType {
-	return &ConnSocketType{
-		conn: conn,
-	}
-}
-
-// CryptSocketType 加密连接
-type CryptSocketType struct {
-	socket SocketInterface
-	crypt  Encryption
-}
-
-func (c *CryptSocketType) Read(ctx context.Context, data []byte) (int, error) {
-	n, err := c.socket.Read(ctx, data)
+func (c *CryptConnType) Read(data []byte) (int, error) {
+	n, err := c.conn.Read(data)
 	if err != nil {
 		return n, err
 	}
@@ -72,18 +19,18 @@ func (c *CryptSocketType) Read(ctx context.Context, data []byte) (int, error) {
 	return n, nil
 }
 
-func (c *CryptSocketType) Write(ctx context.Context, data []byte) (int, error) {
+func (c *CryptConnType) Write(data []byte) (int, error) {
 	c.crypt.Encrypt(data)
-	return c.socket.Write(ctx, data)
+	return c.conn.Write(data)
 }
 
-func (c *CryptSocketType) Close() error {
-	return c.socket.Close()
+func (c *CryptConnType) Close() error {
+	return c.conn.Close()
 }
 
-func NewCryptSocket(socket SocketInterface, encryption Encryption) *CryptSocketType {
-	return &CryptSocketType{
-		socket:     socket,
+func NewCryptSocket(conn io.ReadWriteCloser, encryption Encryption) *CryptConnType {
+	return &CryptConnType{
+		conn:  conn,
 		crypt: encryption,
 	}
 }
